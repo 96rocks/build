044c16d024e0b7402d7312159ec43ae9f69ff67a
brian <brian@vamrs.com>
2020-05-27 16:48:36 +0800

    add zaku board support
    Signed-off-by: brian <brian@vamrs.com>

--- board_configs.sh
+++ board_configs.sh
@@ -52,6 +52,14 @@ case ${BOARD} in
 		export CROSS_COMPILE=aarch64-linux-gnu-
 		CHIP="rk3399"
 		;;
+	"zaku")
+		DEFCONFIG=rockchip_linux_defconfig
+		UBOOT_DEFCONFIG=zaku-rk3399_defconfig
+		DTB=rk3399-zaku-linux.dtb
+		export ARCH=arm64
+		export CROSS_COMPILE=aarch64-linux-gnu-
+		CHIP="rk3399"
+		;;
 	"rockpi4b")
 		DEFCONFIG=rockchip_linux_defconfig
 		DEFCONFIG_MAINLINE=defconfig
